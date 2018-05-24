26527bcedfa3d6b2cf1945bc61a782a628e0de77
chiabi <chiabi88@gmail.com>
2018-05-24 17:25:00 +0900

    스타일 수정, 이벤트 안의 로직 따로 함수로 뺌

--- src/index.js
+++ src/index.js
@@ -19,12 +19,21 @@ function deepCopyTemplate(templateNode) {
   return document.importNode(templateNode, true);
 }
 
+// 로그인
 function login(token) {
   localStorage.setItem('token', token);
 
   todoAPI.defaults.headers['Authorization'] = `Bearer ${token}`;
 }
-// 로그인
+
+// 로그아웃
+function logout() {
+  localStorage.removeItem('token');
+
+  delete todoAPI.defaults.headers['Authorization'];
+}
+
+// 로그인 페이지
 async function loginPage() {
   const fragment = deepCopyTemplate(templates.loginForm);
   fragment.querySelector('.login-form').addEventListener('submit', async e => {
@@ -44,32 +53,93 @@ async function loginPage() {
 async function todoContentPage() {
   const fragment = deepCopyTemplate(templates.todoContent);
   const res = await todoAPI.get('/todos');
+
+  // 리스트 생성
   res.data.forEach(todo => {
-    const itemFragment = deepCopyTemplate(templates.todoItem);
-    const itemEl = itemFragment.querySelector('.todo-item');
-    itemFragment.querySelector('.todo-item__body').textContent = todo.body;
-    itemFragment.querySelector('.todo-item__complete-btn').addEventListener('click', async e => {
-      e.preventDefault();
-      const res = await todoAPI.patch(`/todos/${todo.id}`, {'complete': true});
-    })
-    itemFragment.querySelector('.todo-item__delete-btn').addEventListener('click', async e => {
-      e.preventDefault();
-      const res = await todoAPI.delete(`/todos/${todo.id}`);
-      todoContentPage();
-    })
-    fragment.querySelector('.todo-list').appendChild(itemFragment);
+    todoContentItem(fragment, todo);
   });
-  fragment.querySelector('.todo-form').addEventListener('submit', async e => {
+
+  // 추가 버튼
+  fragment.querySelector('.todo-form').addEventListener('submit', e => {
     e.preventDefault();
-    const payload = {
-      body: e.target.elements.body.value
-    };
-    const res = await todoAPI.post('/todos', payload);
-    todoContentPage();
+    addTodoList(e.target);
   });
+
+  // 완료 전체 삭제 버튼
+  fragment.querySelector('.todo-content__complete-delete-btn').addEventListener('click', e => {
+    e.preventDefault();
+    deleteAllCompleteItems();
+  });
+
+  // 전체 삭제 버튼
+  fragment.querySelector('.todo-content__delete-btn').addEventListener('click', e => {
+    e.preventDefault();
+    deleteAllItems();
+  });
+
+  // 로그아웃 
+  fragment.querySelector('.todo-content__logout-btn').addEventListener('click', e => {
+    logout();
+    loginPage();
+  })
   render(fragment);
 }
 
+// 리스트 추가
+async function addTodoList(eTarget) {
+  const payload = {
+    body: eTarget.elements.body.value,
+    complete: false
+  };
+  const res = await todoAPI.post('/todos', payload);
+  todoContentPage();
+}
+
+// 완료 아이템 모두 삭제
+async function deleteAllCompleteItems() {
+  const res = await todoAPI.get('/todos?complete_ne=false');
+  res.data.forEach(async completeTodo => {
+    await todoAPI.delete(`/todos/${completeTodo.id}`);
+    todoContentPage();
+  });
+}
+
+// 아이템 모두 삭제
+async function deleteAllItems() {
+  const res = await todoAPI.get('/todos');
+  res.data.forEach(async todo => {
+    await todoAPI.delete(`/todos/${todo.id}`);
+    todoContentPage();
+  });
+}
+
+// 할일 각각 아이템
+async function todoContentItem(parentNode, resTodo) {
+  const itemFragment = deepCopyTemplate(templates.todoItem);
+  const itemEl = itemFragment.querySelector('.todo-item');
+  itemFragment.querySelector('.todo-item__body').textContent = resTodo.body;
+  
+  // 완료
+  itemFragment.querySelector('.todo-item__complete-btn').addEventListener('click', async e => {
+    e.preventDefault();
+    const res = await todoAPI.patch(`/todos/${resTodo.id}`, {complete: true});
+    todoContentPage();
+  });
+  
+  // 삭제
+  itemFragment.querySelector('.todo-item__delete-btn').addEventListener('click', async e => {
+    e.preventDefault();
+    const res = await todoAPI.delete(`/todos/${resTodo.id}`);
+    todoContentPage();
+  });
+
+  if (resTodo.complete) {
+    itemEl.classList.add('todo-item--complete');
+    parentNode.querySelector('.todo-complete-list').appendChild(itemFragment);
+  } else {
+    parentNode.querySelector('.todo-list').appendChild(itemFragment);
+  }
+}
 
 if(localStorage.getItem('token')) {
   login(localStorage.getItem('token'));
